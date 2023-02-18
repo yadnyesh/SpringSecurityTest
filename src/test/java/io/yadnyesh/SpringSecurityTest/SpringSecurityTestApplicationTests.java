13fdce03d41549cf58ae04f1409e68c342e2fb3b
yadnyesh <yadnyesh.juvekar@gmail.com>
2023-02-18 11:02:09 +0530

    First Commit

---
+++ src/test/java/io/yadnyesh/SpringSecurityTest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.SpringSecurityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
