bbde207969dcfd0f69932a8a188118d66268ebc3
yadnyesh <yadnyesh.juvekar@gmail.com>
2023-02-19 01:20:23 +0530

    Working Code

---
+++ src/main/java/io/yadnyesh/SpringSecurityTest/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package io.yadnyesh.SpringSecurityTest.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("NORMAL")
+                .build();
+
+        UserDetails adminUser = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(normalUser, adminUser);
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/home/admin")
+                .hasRole("ADMIN")
+                .requestMatchers("/home/normal")
+                .hasRole("NORMAL")
+                .requestMatchers("/home/public")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/io/yadnyesh/SpringSecurityTest/controller/HomeController.java
@@ -0,0 +1,29 @@
+package io.yadnyesh.SpringSecurityTest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @PreAuthorize("hasRole('NORMAL')")
+    @GetMapping("/normal")
+    public ResponseEntity<String> normalUser() {
+        return ResponseEntity.ok("Normal User");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminUser() {
+        return ResponseEntity.ok("Admin User");
+    }
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicUser() {
+        return ResponseEntity.ok("Public User");
+    }
+
+}
